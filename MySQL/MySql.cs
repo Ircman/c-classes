6863ebd00272339530e36e7885b2425ac9ec7e03
Ircman <33596277+Ircman@users.noreply.github.com>
2019-08-30 10:29:13 +0300

    Update MySql.cs

--- MySQL/MySql.cs
+++ MySQL/MySql.cs
@@ -1,4 +1,12 @@
- class DbMySQL
+ using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace ProjectManager.ClassFolder
+{
+    class DbMySQL
     {
         private static string host = "192.168.100.9";
         private static int port = 3306;
@@ -10,12 +18,12 @@
         /// Метод формирует строку соеденнения
         /// </summary>
         /// <returns>Возврашает String с параметрями для соеденения </returns>
-        private static MySqlConnection GetConnection()
+        private static string GetConnection()
         {
             // Connection String.
             String connString = "Server=" + host + ";Database=" + database + ";port=" + port + ";User Id=" + username + ";password=" + password;
-            MySqlConnection connection = new MySqlConnection(connString);
-            return connection;
+            //MySqlConnection connection = new MySqlConnection(connString);
+            return connString;
         }
         /// <summary>
         /// Метод задает параметры для соедененния с базой MySQL
@@ -25,7 +33,7 @@
         /// <param name="databaseName">База данных</param>
         /// <param name="usr">Логин</param>
         /// <param name="pwd">пароль</param>
-        public static void SetConnection(string hostName, int portN, string databaseName,string usr,string pwd)
+        public static void SetConnection(string hostName, int portN, string databaseName, string usr, string pwd)
         {
             host = hostName;
             port = portN;
@@ -42,11 +50,13 @@
         {
             try
             {
-                if (GetConnection() == null)
+                MySqlConnection connection = new MySqlConnection(GetConnection());
+                connection.Open();
+               var s = connection.State.ToString();
+                if (s == "Open")
                 {
                     if (String.IsNullOrEmpty(database))
                         return false;
-                    GetConnection().Open();
                 }
                 return true;
             }
@@ -61,7 +71,7 @@
         /// </summary>
         public void Close()
         {
-            GetConnection().Close();
+            
         }
 
         /// <summary>
@@ -70,27 +80,50 @@
         /// <param name="query">строка запроса</param>
         /// <param name="columnCount">Кол-во возврашаемых параметров</param>
         /// <returns></returns>
-        public List<string[]>QueryList(string query, int columnCount )
+        public List<string[]> QueryList(string query, int columnCount)
         {
             List<string[]> responseSQL = new List<string[]>();
             if (IsConnect())
             {
                 string[] sqlData;
-                var cmd = new MySqlCommand(query, GetConnection());
-                var reader = cmd.ExecuteReader();
-                while (reader.Read())
+             //   var cmd = new MySqlCommand(query, GetConnection());
+              //  var reader = cmd.ExecuteReader();
+           //     while (reader.Read())
                 {
                     sqlData = new string[columnCount];
                     for (int i = 0; i < sqlData.Length; i++)
                     {
-                        sqlData[i] = Convert.ToString(reader.GetValue(i));
+                //        sqlData[i] = Convert.ToString(reader.GetValue(i));
                     }
                     responseSQL.Add(sqlData);
-                }              
-                reader.Close();
+                }
+            //    reader.Close();
             }
             Close();
             return responseSQL;
+        }
+
+        public void Insert(List<string>data)
+        {
+            Crc32  crc32 = new Crc32();
+            
+            if (IsConnect())
+            {
+                MySqlConnection connection = new MySqlConnection(GetConnection());
+                connection.Open();
+                foreach (var path in data)
+                {
+                    string hash = crc32.GetFileCrc32(path);
+                    string query = string.Format("INSERT INTO test (filename,crc) VALUES ('{0}', '{1}');",path,hash);
+                   
+
+                    MySqlCommand cmd = connection.CreateCommand();
+                    cmd.CommandText = query;
+                    cmd.Parameters.Add(path, hash);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            Close();
         }
     }
 }
