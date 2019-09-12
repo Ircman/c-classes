c2897f5cf56e1dbc21873a29d92e2b8cb8eb2e2e
Ircman <33596277+Ircman@users.noreply.github.com>
2019-09-12 14:55:02 +0300

    Update ERROR_last.cs

--- ERROR compoent/ERROR_last.cs
+++ ERROR compoent/ERROR_last.cs
@@ -1,4 +1,11 @@
-public static void Msg(Exception ex)
+ public static class Error
+    {
+        static readonly DbMySQL MySql = new DbMySQL();
+        /// <summary>
+        /// Метод выдает ошибку и записавает ее базу данных
+        /// </summary>
+        /// <param name="ex">exeption</param>
+        public static void Msg(Exception ex)
         {
             StackFrame frame = new StackFrame(1);
             var method = frame.GetMethod();
@@ -19,3 +26,26 @@ public static void Msg(Exception ex)
                 MySql.ErrorMsg(className, metodName, msg, innerEx,line);
                 MessageBox.Show(String.Format("Произошла ошибка\n Название класса: {0}\nНазвание метода:{1}\nПричина:{2},\n номер линии :{4} \nчто вызвало:{3}", className, metodName, msg, innerEx, line), @"Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+        /// <summary>
+        /// Метод получает номер строки
+        /// </summary>
+        /// <param name="ex">exeption</param>
+        /// <returns>int line number</returns>
+        private static int GetLineNumber(Exception ex)
+        {
+            var lineNumber = 0;
+            const string lineSearch = ":line ";
+            var index = ex.StackTrace.LastIndexOf(lineSearch);
+            if (index != -1)
+            {
+                var lineNumberText = ex.StackTrace.Substring(index + lineSearch.Length);
+                if (int.TryParse(lineNumberText, out lineNumber))
+                {
+                }
+            }
+            return lineNumber;
+        }
+
+    
+    }
