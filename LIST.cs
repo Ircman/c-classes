73f9dd29fec4fe0f23a639c4de31cd0035bcd3ad
Ircman <33596277+Ircman@users.noreply.github.com>
2019-10-02 16:18:58 +0300

    Update LIST.cs

--- LIST.cs
+++ LIST.cs
@@ -1,3 +1,16 @@
 var itemToRemove = _selectedPackages.Packages.Single(r => r.id == Convert.ToInt32(sqLreponse[i][0]));
                         if (itemToRemove != null)
                         _selectedPackages.Packages.Remove(itemToRemove);
+
+
+// A function to search for people
+Person FindPerson(int id) {
+   var people = DbContext.GetPeople(); // Returns List<Person>
+
+   return (from person in people
+          where person.ID == id
+          select person).ToList().FirstOrDefault();
+}
+
+// Then do this
+var person = FindPerson(123);
