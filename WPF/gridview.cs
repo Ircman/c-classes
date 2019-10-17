39544660375918437d998ae635169ffc482f1baf
Ircman <33596277+Ircman@users.noreply.github.com>
2019-10-17 17:33:40 +0300

    Create gridview.cs

---
+++ WPF/gridview.cs
@@ -0,0 +1,19 @@
+// <summary>
+        /// Метод получает строку 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dg_datagrid_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+
+            Class1 s;
+            var cellInfo = dg_datagrid.SelectedCells[0];
+
+            s = (Class1)cellInfo.Item;
+            var tmp = s.test2;
+            
+
+            var content = cellInfo.Column.GetCellContent(cellInfo.Item);
+           
+            ;
+        }
