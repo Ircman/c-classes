3a56f85cd1c21ccb76a7b9fc36e51b7a4b9e80f5
Ircman <33596277+Ircman@users.noreply.github.com>
2019-12-05 13:39:10 +0200

    Update invoke.cs
    добавлен новый способ доступа универсальный

--- Threads/invoke.cs
+++ Threads/invoke.cs
@@ -6,3 +6,33 @@ if (InvokeRequired)
                         infoForm.Hide();
                     }));
                 }
+
+
+----------------------------------------------------- второй способ
+  delegate void SetControlValueCallback(Control oControl, string propName, object propValue);
+
+        private void SetControlPropertyValue(Control oControl, string propName, object propValue)
+
+        {
+            if (oControl.InvokeRequired)
+
+            {
+                SetControlValueCallback d = SetControlPropertyValue;
+
+                oControl.Invoke(d, oControl, propName, propValue);
+            }
+
+            else
+
+            {
+                var t = oControl.GetType();
+
+                var props = t.GetProperties();
+
+                foreach (var p in props)
+
+                    if (p.Name.ToUpper() == propName.ToUpper())
+
+                        p.SetValue(oControl, propValue, null);
+            }
+        }
